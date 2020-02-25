c8618a57598210948ed42a625ba2bf95c877065e
dillonwi <46857484+dillonwi@users.noreply.github.com>
2020-02-24 21:51:54 -0700

    Add files via upload

--- tests.py
+++ tests.py
@@ -15,29 +15,26 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
-
-if __name__ == '__main__':
-    unittest.main()
-
-
-class Test(TestCase):
     def test_get_area(self):
         #Initialize an area equal to 9 x pi.
         tArea = 9 * math.pi
 
-        #Check to see that 9pi is equal to what comes out of passing 3 into the getArea function.
+         # Check to see that 9pi is equal to what comes out of passing 3 into the getArea function.
         self.assertEqual(tArea, task.getArea(3))
 
-        #Set tArea to 25 pi
+        # Set tArea to 25 pi
         tArea = 25 * math.pi
 
-        #Check the running getArea with radius 5 returns 25 pi
+        # Check the running getArea with radius 5 returns 25 pi
         self.assertEqual(tArea, task.getArea(5))
 
-        #Set tArea to 0
+        # Set tArea to 0
         tArea = 0
 
         # Check the running getArea with radius 0 returns 0
         self.assertEqual(tArea, task.getArea(0))
-        
+
         pass
+
+if __name__ == '__main__':
+    unittest.main()
