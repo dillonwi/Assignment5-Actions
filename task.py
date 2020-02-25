d5145e1d3309baad389f567115fc22e951d77395
dillonwi <46857484+dillonwi@users.noreply.github.com>
2020-02-24 21:46:38 -0700

    Add files via upload

--- task.py
+++ task.py
@@ -1,2 +1,16 @@
+import math
+
+
 def firstrun():
     return "success"
+
+
+def getArea(radius):
+    # Initialize the area variable.
+    area = 0
+
+    # Mulitply the radius by itself and pi to get the area
+    area = radius * radius * math.pi
+
+    # return the area
+    return area
--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 import unittest
+from unittest import TestCase
+import math
+
 import task
 
 
@@ -15,3 +18,26 @@ class TestCase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+class Test(TestCase):
+    def test_get_area(self):
+        #Initialize an area equal to 9 x pi.
+        tArea = 9 * math.pi
+
+        #Check to see that 9pi is equal to what comes out of passing 3 into the getArea function.
+        self.assertEqual(tArea, task.getArea(3))
+
+        #Set tArea to 25 pi
+        tArea = 25 * math.pi
+
+        #Check the running getArea with radius 5 returns 25 pi
+        self.assertEqual(tArea, task.getArea(5))
+
+        #Set tArea to 0
+        tArea = 0
+
+        # Check the running getArea with radius 0 returns 0
+        self.assertEqual(tArea, task.getArea(0))
+        
+        pass
